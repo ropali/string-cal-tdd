2f6be6ec0465e5dd77544c1dc731b9104d0b53ff
ropali <ropali68@gmail.com>
2025-02-24 19:09:13 +0530

    feat: handle single number input in add method of StringCalculator

--- string_calculator.py
+++ string_calculator.py
@@ -3,3 +3,6 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        
+        if len(numbers) == 1:
+            return int(numbers)
