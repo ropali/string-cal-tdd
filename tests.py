0f6ed8bdeeb4a65d23e6133ab99f98ff351b4cb0
ropali <ropali68@gmail.com>
2025-02-24 19:07:15 +0530

    feat: add test for single number input in StringCalculator

--- tests.py
+++ tests.py
@@ -4,3 +4,9 @@ from string_calculator import StringCalculator
 def test_empty_string():
     calculator = StringCalculator()
     assert calculator.add("") == 0
+
+
+def test_single_number():
+    calculator = StringCalculator()
+    
+    assert calculator.add("1") == 1
